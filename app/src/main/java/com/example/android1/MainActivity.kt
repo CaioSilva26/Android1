54d6d30ae409962f78b1770b78a17f28933c8090
CaioSilvaLmaooo <caiosilvadeazevedo@gmail.com>
2023-09-21 09:45:26 -0300

    Lmaooooo

--- app/src/main/java/com/example/android1/MainActivity.kt
+++ app/src/main/java/com/example/android1/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Android1Theme {
-        Greeting("Era uma vez um pato viuvo")
+        Greeting("Era uma vez um pato viuvo sem a pata")
     }
 }
